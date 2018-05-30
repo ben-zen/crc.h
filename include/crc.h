488556ca66298319503dc3fb6b5c77a6f0d5bf12
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2018-05-30 13:40:57 +0300

    Fixed CRC calculation for (hopefully) all combinations of parameters

--- include/crc.h
+++ include/crc.h
@@ -38,43 +38,44 @@ namespace crc
         static constexpr crc_t msb_mask = 1 << (size - 1);
         static constexpr crc_t full_mask = static_cast<crc_t>((static_cast<crc_t>(1) << size) - 1);
 
-        static_assert(static_cast<crc_t>(static_cast<crc_t>(1) << (size - 1)) != 0, "The provided data type can't fit the CRC size requested");
         static_assert(size > 0, "CRC size must be greater than 0");
+        static_assert(static_cast<crc_t>(static_cast<crc_t>(1) << (size - 1)) != 0, "The provided data type can't fit the CRC size requested");
         static_assert((polynomial & full_mask) == polynomial, "Polynomial is larger than the specified CRC size");
         static_assert((initial & full_mask) == initial, "Initial value is larger than the specified CRC size");
         static_assert((final_xor & full_mask) == final_xor, "Final xor value is larger than the specified CRC size");
-        static_assert((!reflect_input && !reflect_output) || (size % 8 == 0), "Calculating reflected checksums for non-multiples of 8 currently not supported");
 
         crc_t current;
 
-        static constexpr uint8_t reflect_byte(uint8_t b)
+        static constexpr uint8_t reflect_byte(uint8_t b, size_t len)
         {
-            /*uint8_t result = 0;
-            for(auto i = 0; i < 8; ++i)
+            uint8_t result = 0;
+            for(auto i = 0; i < len; ++i)
             {
                 result <<= 1;
                 result |= (b & 1);
                 b >>= 1;
             }
-            return result;*/
+            return result;
             
-            b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+            /*b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
             b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-            b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+            b = (b & 0xAA) >> 1 | (b & 0x55) << 1;*/
             return b;
         }
 
         static constexpr crc_t reflect_crc(crc_t crc)
         {
+            size_t remaining_size = size;
             crc_t reflected = 0;
             for(auto i = 0; i < size; i += 8)
             {
+                size_t len = remaining_size < 8 ? remaining_size : 8;
+                remaining_size -= len;
                 auto byte = static_cast<uint8_t>(crc & 0xFF);
-                auto reflected_byte = reflect_byte(byte);
-                //reflected |= reflected_byte << i;
-                reflected <<= 8;
+                auto reflected_byte = reflect_byte(byte, len);
+                reflected <<= len;
                 reflected |= reflected_byte;
-                crc >>= 8;
+                crc >>= len;
             }
             return reflected;
         }
@@ -107,7 +108,7 @@ namespace crc
                 table_t refl{};
                 for (auto i = 0; i < table_size; ++i)
                 {
-                    auto refl_offset = reflect_byte(static_cast<uint8_t>(i));
+                    auto refl_offset = reflect_byte(static_cast<uint8_t>(i), 8);
                     auto refl_entry = reflect_crc(table[refl_offset]);
                     refl[i] = refl_entry;
                 }
